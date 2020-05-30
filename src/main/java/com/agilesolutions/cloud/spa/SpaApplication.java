d783cf7a07d0665bf031cea8d434450ef70be634
admin <rong@agile-solutions.ch>
2020-05-30 13:20:30 +0200

    home grown api gateway based on generic

--- src/main/java/com/agilesolutions/cloud/spa/SpaApplication.java
+++ src/main/java/com/agilesolutions/cloud/spa/SpaApplication.java
@@ -1,27 +1,75 @@
 package com.agilesolutions.cloud.spa;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @SpringBootApplication
 public class SpaApplication {
 	
+	@Value("${app.services.party-service.url:https://mycomp.com/api/party/getall}")
+	private String partyServiceUrl;
+	
 
 	@RequestMapping("/hystrixfallback")
 	public String hystrixfallback() {
 		return "This is a fallback";
+	}
+	
+	/**
+	 * All party traffic goes here, make your own gateway here if you can not deal with webflux
+	 * 
+	 */
+	@RequestMapping(value = { "/party/**" }, method = { RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST })
+	public ResponseEntity<?> partyRequests(@RequestBody(required = false) String body, HttpMethod method,
+			HttpServletRequest request, HttpServletResponse response) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		try {
+			// compose new forwardable URL to down-stream service
+			URI uri = UriComponentsBuilder.fromUri(new URI(partyServiceUrl)).path(request.getRequestURI()).query(request.getQueryString()).build(true).toUri();
+			
+			HttpHeaders headers = new HttpHeaders();
+
+			// add Kerberos Constrained delegated ticket
+			headers.add(HttpHeaders.AUTHORIZATION,"Negotiate " + "YOURTICKET");
+			
+			HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+			
+			return restTemplate.exchange(uri, method, httpEntity, String.class);
+		
+		} catch (RestClientException | URISyntaxException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+
 	}
 
 	@Bean
